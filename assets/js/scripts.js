46cf2dd8645f8542d2b86e48a3b48a613941b417
eslamansour <eslam.mansour15@gmail.com>
2021-03-30 06:33:28 +0200

    add social media icons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,6 @@
 (function ($) {
   "use strict";
 
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
   $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
@@ -32,19 +19,4 @@
   };
   navbarCollapse();
   $(window).scroll(navbarCollapse);
-
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
 })(jQuery);
